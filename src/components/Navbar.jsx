ea7093dba33ebf5ccb21542c5e9845f5d55216c2
Christopher Phillips <csphilli@gmail.com>
2022-04-30 16:27:32 +0300

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,13 @@ import { NavData } from "./NavData";
 import { HiMenu } from "react-icons/hi";
 
 function Navbar({ pageId }) {
-    // const [navMenu, setNavMenu] = useState(false);
-
-    // const onNavClick = () => {
-    //     console.log("nav link clicked");
-    //     setNavMenu(!navMenu);
-    // };
-
     return (
         <nav className={styles.navBar}>
             <h1 className={styles.logoText}>Project3 Studio</h1>
-            {/* <HiMenu
-                onClick={() => setNavMenu(!navMenu)}
-                className={styles.navMenuIcon}
-            /> */}
-            <div
-                // className={
-                //     navMenu
-                //         ? `${styles.navMenu} ${styles.menuActive}`
-                //         : styles.navMenu
-                // }
-                className={`${styles.navBar} ${styles.links}`}
-            >
+            <div className={styles.links}>
                 {NavData.map((item, index) => {
                     return (
                         <Link
-                            // onClick={() => onNavClick()}
                             key={index}
                             className={
                                 pageId === item.name ? styles.active : "none"
