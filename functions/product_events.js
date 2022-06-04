a1b3c44ebb5385efad99a7ab47bebdf3f8b7da7c
Christopher Phillips <csphilli@gmail.com>
2022-06-04 13:57:59 +0300

    should be production ready

--- functions/product_events.js
+++ functions/product_events.js
@@ -9,53 +9,49 @@ const { createClient } = require("@supabase/supabase-js");
 const getSupabaseClient = (url, key) => {
     return createClient(url, key);
 };
-const supabaseEU = getSupabaseClient(
-    process.env.GATSBY_SUPABASE_EU_URL,
-    process.env.GATSBY_SUPABASE_EU_KEY
+const supabase = getSupabaseClient(
+    process.env.GATSBY_SUPABASE_URL,
+    process.env.GATSBY_SUPABASE_KEY
 );
 
-const endpointSecret =
-    "whsec_0286817c32a272cf74a6260d4ebcf58cfa2dc19c850cb0c4ff8d24f734d9999b";
+// const endpointSecret =
+//     "whsec_0286817c32a272cf74a6260d4ebcf58cfa2dc19c850cb0c4ff8d24f734d9999b";
 
-const incProducts = [
-    {
-        product_id: "prod_123kdfj3is",
-        default_price: "price_923l4jdf",
-        name: "birdhouse v1",
-        desc: "a charming little birdhouse",
-        images: ["www.youtube.com"],
-        url: "www.localhost8888/products",
-        active: true,
-        created: 1654268973,
-        updated: 1654268973,
-        metadata: {
-            max_qty: 3,
-            inventory: 1,
-            product_type: "digital",
-        },
-    },
-];
+// const incProducts = [
+//     {
+//         product_id: "prod_elite",
+//         default_price: "price_923sdfl4jdf",
+//         name: "birdhouse v1",
+//         desc: "a charming little birdhouse",
+//         images: ["www.youtube.com"],
+//         url: "www.localhost8888/products",
+//         active: true,
+//         metadata: {
+//             max_qty: 3,
+//             inventory: 1,
+//             product_type: "digital",
+//         },
+//     },
+// ];
 
 // To cut down on DB lookup times, the categories here represent the exact category names in the product_catetory table as well as, and most importantly, the exact order they are in.
 const categories = ["digital", "physical"];
 
 const insertIntoInventory = async (max_qty, inv_amount, prod_id) => {
-    const { data, error } = await supabaseEU
+    const { data, error } = await supabase
         .from("product_inventory")
         .insert([
             { max_qty: max_qty, inventory: inv_amount, product_id: prod_id },
         ]);
-    if (data) {
-        return data[0].id;
-    }
     if (error) {
         return {
-            statusCode: 400,
+            statusCode: 500,
             body: JSON.stringify(
                 `Unable to insert inventory for product '${prod_id} for error ${error}'`
             ),
         };
     }
+    return data[0].id;
 };
 
 exports.handler = async (event) => {
@@ -65,60 +61,92 @@ exports.handler = async (event) => {
             event.headers["stripe-signature"],
             endpointSecret
         );
+
         switch (stripeEvent.type) {
-            // Change to proper event type later
-            case "setup_intent.created": {
-                // Getting the category ID her a simple array indexOf function against the categories const above. It returns the value +1 since the DB id is not index 0.
+            // This will be product.created
+            // case "setup_intent.created": {
+            case "product.created": {
+                const product = JSON.parse(event.body).data;
+                // Getting the category ID here is a simple array indexOf function against the categories const above. It returns the value +1 since the DB id is not index 0. Requires a small bit of maintenance since it's housed here instead of checking the DB but improves performance significantly.
                 const category_id =
-                    categories.indexOf(incProducts[0].metadata.product_type) +
-                    1;
+                    categories.indexOf(product.metadata.product_type) + 1;
                 if (category_id === 0) {
                     throw new Error(
-                        `Category '${incProducts[0].metadata.product_type}' doesn't exist in categories array`
+                        `Category '${product.metadata.product_type}' doesn't exist in categories array`
                     );
                 }
 
                 // inserting into inventory table
-                console.time("inventory_id");
                 const inventory_id = await insertIntoInventory(
-                    incProducts[0].metadata.max_qty,
-                    incProducts[0].metadata.inventory,
-                    incProducts[0].product_id
+                    product.metadata.max_qty,
+                    product.metadata.inventory,
+                    product.product_id
                 );
-                console.timeEnd("inventory_id");
 
                 // Inserting into products table when product.created webhook fires.
-                console.time("products");
-                const { data, error } = await supabaseEU
-                    .from("products")
-                    .insert([
-                        {
-                            product_id: incProducts[0].product_id,
-                            default_price: incProducts[0].default_price,
-                            name: incProducts[0].name,
-                            desc: incProducts[0].desc,
-                            category_id: category_id,
-                            inventory_id: inventory_id,
-                            image_url: incProducts[0].images[0],
-                            project_url: incProducts[0].url,
-                            created: new Date(incProducts[0].created * 1000),
-                            updated: new Date(incProducts[0].updated * 1000),
-                            active: incProducts[0].active,
-                            likes: 0,
-                            like_level: 0,
-                        },
-                    ]);
-                console.timeEnd("products");
+
+                const { error } = await supabase.from("products").insert([
+                    {
+                        product_id: product.product_id,
+                        default_price: product.default_price,
+                        name: product.name,
+                        desc: product.desc,
+                        category_id: category_id,
+                        inventory_id: inventory_id,
+                        image_url: product.images[0],
+                        project_url: product.url,
+                        active: product.active,
+                        likes: 0,
+                        like_level: 0,
+                    },
+                ]);
                 if (error) {
                     throw new Error(
-                        `Could not insert new row in 'products' for ${incProducts[0].product_id}: ${error}`
+                        `Could not insert new row in 'products' for ${product.product_id}: ${error}`
+                    );
+                }
+                break;
+            }
+            // This will be product.updated
+            // Will only allow changes on the following columns. If there is a change to the other column, it will instead be a new product being created. Can't change the product_id, category_id, inventory_id, likes, or like_lvl.
+            case "product.updated": {
+                // case "placeholder2": {
+                const product = JSON.parse(event.body).data;
+                const { error } = await supabase
+                    .from("products")
+                    .update({
+                        default_price: product.default_price,
+                        name: "testing update",
+                        desc: product.desc,
+                        image_url: "www.project3designs.com",
+                        project_url: product.url,
+                        active: false,
+                    })
+                    .eq("product_id", product.product_id);
+                if (error) {
+                    throw new Error(
+                        `Could not update row for ${product.product_id}: ${error}`
+                    );
+                }
+                break;
+            }
+            case "product.deleted": {
+                // case "setup_intent.created": {
+                const product = JSON.parse(event.body).data;
+                const { error } = await supabase
+                    .from("products")
+                    .delete()
+                    .eq("product_id", product.product_id);
+                if (error) {
+                    throw new Error(
+                        `Could not delete product: ${product.product_id}: ${error}`
                     );
                 }
                 break;
             }
 
             default: {
-                console.error(
+                throw new Error(
                     `No matching webhooks in product_events function for ${stripeEvent.type}`
                 );
             }
@@ -126,9 +154,10 @@ exports.handler = async (event) => {
 
         return {
             statusCode: 200,
-            body: JSON.stringify(`Tuna add to DB`),
+            body: JSON.stringify({ result: true }),
         };
     } catch (error) {
+        console.error(error);
         return {
             statusCode: 500,
             body: JSON.stringify(error),
