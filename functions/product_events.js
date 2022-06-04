5dad9ffc6a356b325cb133b0ad46c2ec6e6cca8f
Christopher Phillips <csphilli@gmail.com>
2022-06-04 20:06:58 +0300

    test

--- functions/product_events.js
+++ functions/product_events.js
@@ -70,12 +70,7 @@ exports.handler = async (event) => {
             // case "setup_intent.created": {
             case "product.created": {
                 const { product } = JSON.parse(event.body).data.object;
-                console.log(
-                    "ID",
-                    product.product_id,
-                    "PRICE",
-                    product.default_price
-                );
+                console.log(product);
 
                 // Getting the category ID here is a simple array indexOf function against the categories const above. It returns the value +1 since the DB id is not index 0. Requires a small bit of maintenance since it's housed here instead of checking the DB but improves performance significantly.
                 const category_id =
