bac4df58f4be6e8b09c4f08f9cbea0ffc0da7c72
Christopher Phillips <csphilli@gmail.com>
2022-07-13 11:17:40 +0300

    created component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 import * as styles from "../scss/carousel.module.scss";
 
 function Carousel(props) {
-    const { slug } = props;
-    console.log(slug);
+    const { images } = props;
 
-    return <div>Carousel</div>;
+    if (images.length > 0) {
+        return (
+            <Splide
+                aria-label="Our Favorite Images"
+                className={styles.slider_container}
+                options={{
+                    rewind: true,
+                    gap: "1rem",
+                    speed: 1000,
+                    height: "30rem",
+                    width: "100%",
+                }}
+            >
+                {images.map((item, index) => {
+                    const image = getImage(item);
+                    return (
+                        <SplideSlide key={index}>
+                            <GatsbyImage
+                                key={index}
+                                image={image}
+                                className={styles.image}
+                                alt={`Slide image ${index}`}
+                            />
+                        </SplideSlide>
+                    );
+                })}
+            </Splide>
+        );
+    } else return null;
 }
 
 export default Carousel;
