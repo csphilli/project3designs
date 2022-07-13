d5a8ba4d0ebe0255b0ecb06dbcc4fdb796ff400b
Christopher Phillips <csphilli@gmail.com>
2022-07-13 11:15:00 +0300

    deleting unneeded

--- src/components/ShoppingCart.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import * as styles from "../scss/shoppingCart.module.scss";
-
-function ShoppingCart() {
-    return <div>ShoppingCart</div>;
-}
-
-export default ShoppingCart;
