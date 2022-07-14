f9b2a7046b42788ebc76c1cf89d5c1121314076e
Christopher Phillips <csphilli@gmail.com>
2022-07-14 09:16:50 +0300

    added try/catch

--- functions/getProduct.js
+++ functions/getProduct.js
@@ -9,39 +9,49 @@ const supabase = createClient(
 );
 
 exports.handler = async (data) => {
-    const body = JSON.parse(data.body);
-    const { search } = body;
+    try {
+        const body = JSON.parse(data.body);
+        const { search } = body;
 
-    // Verifying form data is from P3D site
-    const header = data.headers;
-    const token = header && header.authorization.split(" ")[1];
-    if (token === null) throw new Error(MESSAGES.INVALID_TOKEN);
-    jwt.verify(token, process.env.P3D_SIGNATURE_KEY);
+        // Verifying form data is from P3D site
+        const header = data.headers;
+        const token = header && header.authorization.split(" ")[1];
+        if (token === null) throw new Error("Missing P3D Auth Token");
+        jwt.verify(token, process.env.P3D_SIGNATURE_KEY);
 
-    const { data: products, error } = await supabase
-        .from("products")
-        .select("*")
-        .eq("p3_id", search)
-        .eq("active", true)
-        .order("updated", { ascending: false })
-        .order("inventory", { ascending: false });
+        const { data: products, error } = await supabase
+            .from("products")
+            .select("*")
+            .eq("p3_id", search)
+            .eq("active", true)
+            .order("updated", { ascending: false })
+            .order("inventory", { ascending: false });
 
-    if (error) {
+        return {
+            statusCode: 200,
+            // body: JSON.stringify(products),
+            body: JSON.stringify({ status: 200, data: products }),
+        };
+    } catch (error) {
         console.log(error);
 
         return {
             statusCode: 400,
-            body: JSON.stringify({
-                error: `Could not fetch products from supabase from get_products function`,
-            }),
+            body: JSON.stringify(error),
         };
     }
 
-    // console.log([...products]);
+    // if (error) {
+    //     console.log(`ERROR! ${error}`);
+    //     throw new Error(error);
 
-    return {
-        statusCode: 200,
-        // body: JSON.stringify(products),
-        body: JSON.stringify({ status: 200, data: products }),
-    };
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify({
+    //             error: `Could not fetch products from supabase from get_products function`,
+    //         }),
+    //     };
+    // }
+
+    // console.log([...products]);
 };
